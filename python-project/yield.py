fdcd2e1abc789e597e56ec49becba4be4521a7f6
Valentin Vetter <valentin@codingame.com>
2017-06-15 15:26:38 +0200

    Rename stub files

--- python-project/yield.py
+++ python-project/yield.py
@@ -1,11 +1,11 @@
-def fibonacci_gen():
+def fibonacci_generator():
     a, b = 0, 1
     while True:
         yield a
         a, b = b, a + b
 
 # Print all the numbers of the Fibonacci sequence that are lower than 1000
-for i in fibonacci_gen():
+for i in fibonacci_generator():
     if i > 1000:
         break
     print(i)
